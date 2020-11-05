768e63a06286d84ba579a97e15cad259a6874dee
Nithish Pravin <72058736+Major-Thrust@users.noreply.github.com>
2020-11-05 23:49:38 +0530

    initial commit

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo "Installing packages required to run the automatic google meet class attender"
+@echo "Meanwhile consider subscribing to my youtube channel"
+@echo "https://www.youtube.com/channel/UC_M6GWo7ZcmUVzssGi7i5Iw"
+
+npm install figlet puppeteer-extra puppeteer-extra-plugin-stealth puppeteer --unsafe-perm=true --allow-root
+
+
+PAUSE
